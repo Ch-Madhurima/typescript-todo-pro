695b9ea8228ad9456610b232a47e93b93688742a
Madhurima30 <ch.madhurimaa@gmail.com>
2022-10-26 14:18:49 -0500

    removing a todo
    removing a todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,14 @@
 import Todo from "../models/Todo";
 import classes from "./TodoItem.module.css";
 
-const TodoItem: React.FC<{ text: Todo["text"] }> = (props) => {
-  return <li className={classes.item}>{props.text}</li>;
+const TodoItem: React.FC<{ text: Todo["text"]; onRemoveTodo: () => void }> = (
+  props
+) => {
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
+  );
 };
 
 export default TodoItem;
